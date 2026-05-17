c020e94163ee889d0551928059b7e5dc0ead31c3
Renat Kapenov <32762032+KapenovRenat@users.noreply.github.com>
2026-05-17 14:36:10 +0500

    [master] added video

--- index.php
+++ index.php
@@ -231,7 +231,7 @@
             </div>
             <div class="video-wrapper">
                 <iframe
-                    src="https://www.youtube.com/watch?v=QAv7FiTuTHE"
+                    src="https://youtu.be/QAv7FiTuTHE?si=xjoTtTJYBUBlv8Y-"
                     title="Производство Феникс Мебель — видео с фабрики"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
