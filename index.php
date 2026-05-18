16380ec33014f97053459687131efd3673722d16
Renat Kapenov <32762032+KapenovRenat@users.noreply.github.com>
2026-05-18 15:05:37 +0500

    [master] updates

--- index.php
+++ index.php
@@ -264,10 +264,33 @@
                     </div>
                 </div>
 
-                <!-- Строка 3: RADEYA -->
-                <div class="partners-row partners-row--1">
+                <div class="partners-row">
+                    <div class="partner-logo fade-in">
+                        <img src="./img/partners/Pravila-mebel.png" alt="Pravila-mebel">
+                    </div>
+
                     <div class="partner-logo fade-in partner-logo--radeya">
                         <img src="./img/partners/Radeya.png" alt="Radeya">
+                    </div>
+
+                    <div class="partner-logo fade-in mebel-three">
+                        <img src="./img/partners/mebel-three.png" alt="mebel-three">
+                    </div>
+                </div>
+
+                <!-- Строка 3: RADEYA -->
+                <div class="partners-row">
+
+                    <div class="partner-logo fade-in">
+                        <img src="./img/partners/dantone.png" alt="dantone">
+                    </div>
+
+                    <div class="partner-logo fade-in">
+                        <img src="./img/partners/mebelgold.png" alt="mebelgold">
+                    </div>
+
+                    <div class="partner-logo fade-in">
+                        <img src="./img/partners/domdivanov.png" alt="domdivanov">
                     </div>
                 </div>
 
@@ -401,7 +424,7 @@
                     </li>
                     <li>
                         <svg viewBox="0 0 24 24" width="14" height="14" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.61 3.41 2 2 0 0 1 3.6 1.21h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.84a16 16 0 0 0 6 6l.86-.86a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 21.21 16.92z"/></svg>
-                        +7 (383) 000-00-00
+                        +7 (902) 812-70-55
                     </li>
                     <li>
                         <svg viewBox="0 0 24 24" width="14" height="14" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>
